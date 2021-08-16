777fa1e28609f88890faec877e787a50f155ee7f
Jishnu <58329238+jishnuk25@users.noreply.github.com>
2021-08-16 17:50:43 +0530

    Update testjob.groovy

--- vars/testjob.groovy
+++ vars/testjob.groovy
@@ -1,8 +1,8 @@
 def call(String name = 'user') {
   pipeline {
-    
+    agent any
     stages {
-      stage('test-shared-libraries') {
+      stage("test-shared-libraries") {
         steps {
              echo "Welcome, ${name}."
         }
