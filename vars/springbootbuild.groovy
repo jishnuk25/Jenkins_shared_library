377d74e820d88119dbdaa6ec4b929e89a7dedbab
Jishnu <58329238+jishnuk25@users.noreply.github.com>
2023-05-02 14:02:31 +0530

    Update springbootbuild.groovy

--- vars/springbootbuild.groovy
+++ vars/springbootbuild.groovy
@@ -1,35 +1,37 @@
 
 def call(String repoUrl) {
   pipeline {
-       agent any
+    agent {
+      label 'master'
         }
-       stages {
-           stage("Tools initialization") {
-               steps {
-                   sh "mvn --version"
-                   sh "java -version"
-               }
-           }
-           stage("Checkout Code") {
-               steps {
-                   git branch: 'master',
-                       url: "${repoUrl}"
-               }
-           }
-           stage("Cleaning workspace") {
-               steps {
-                   sh "mvn clean"
-               }
-           }
-           stage("Running Testcase") {
-              steps {
-                   sh "mvn test"
-               }
-           }
-           stage("Packing Application") {
-               steps {
-                   sh "mvn package -DskipTests"
-               }
-           }
-       }
-   }
+     }
+     stages {
+         stage("Tools initialization") {
+             steps {
+                 sh "mvn --version"
+                 sh "java -version"
+             }
+         }
+         stage("Checkout Code") {
+             steps {
+                 git branch: 'master',
+                     url: "${repoUrl}"
+             }
+         }
+         stage("Cleaning workspace") {
+             steps {
+                 sh "mvn clean"
+             }
+         }
+         stage("Running Testcase") {
+            steps {
+                 sh "mvn test"
+             }
+         }
+         stage("Packing Application") {
+             steps {
+                 sh "mvn package -DskipTests"
+             }
+         }
+     }
+ }
