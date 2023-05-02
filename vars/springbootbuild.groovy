36755bcf72fb15ca13890b367c4ec7c89e82c39f
Jishnu <58329238+jishnuk25@users.noreply.github.com>
2023-05-02 14:34:09 +0530

    Update springbootbuild.groovy

--- vars/springbootbuild.groovy
+++ vars/springbootbuild.groovy
@@ -1,13 +1,13 @@
 def call(String repoUrl) {
   pipeline {
-    agent any
+    
      stages {
-         stage("Tools initialization") {
-             steps {
-                 sh "mvn --version"
-                 sh "java -version"
-             }
-         }
+        //  stage("Tools initialization") {
+        //      steps {
+        //          sh "mvn --version"
+        //          sh "java -version"
+        //      }
+        //  }
          stage("Checkout Code") {
              steps {
                  git branch: 'master',
